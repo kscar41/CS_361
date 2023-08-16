9401165e85bca64dad196b8a98fda8fc08227471
kscar41 <76128620+kscar41@users.noreply.github.com>
2023-08-16 15:03:36 -0400

    Update server.py
    current microservice

--- server.py
+++ server.py
@@ -1,25 +1,26 @@
+# #Implementation of Microservice for Partner Project
 import os
+import threading
+import time
 
-def print_car_log_data(file_name):
-    if os.path.exists(file_name):
-        with open(file_name, "r") as file:
-            car_log_data = file.read()
-            print("Car Log Data:")
-            print(car_log_data)
-    else:
-        print("Car Log does not exist.")
+def findBook():
+    check_search = open('findBook.txt', 'r')
+    with check_search:
+        check = check_search.read().lower()
+        check_search.close()
+        if check == 'searching':
+            file = open('findBook.txt', 'w')
+            file.close()
+            book = open('books.txt', 'r')
+            with book:
+                book_info = book.read().lower()
+                returnBook = book_info
+                with open('foundBook.txt', 'w') as file:
+                    file.write(returnBook)
 
-def find_car_log():
-    car_type = input("Enter the Year, Make, and Model of the car: ")
-
-    if not car_type:
-        print("Invalid input.")
-        return
-
-    car_log = car_type.replace(" ", ".")
-    file_name = car_log + ".txt"
-
-    print_car_log_data(file_name)
-
-if __name__ == "__main__":
-    find_car_log()
+def interval_function():
+    while True:
+        findBook()
+        time.sleep(3)
+interval_thread = threading.Thread(target=interval_function)
+interval_thread.start()
